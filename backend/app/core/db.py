f415a27326ecd8b8e8ea22b237730b5b498ff30b
Maciej <maciej.szok12@gmail.com>
2024-06-28 13:35:57 +0200

    feat: db fixtures with more users

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -2,14 +2,53 @@ from sqlmodel import Session, create_engine, select
 
 from app import crud
 from app.core.config import settings
-from app.models import User, UserCreate
+from app.models import User, UserCreate, Friend
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+# to start with an empty database:
+# docker volume ls
+# find the volume name that corresponds to the database, probably snapi_app-db-data
+# docker volume rm snapi_app-db-data
+
 
 
 # make sure all SQLModel models are imported (app.models) before initializing DB
 # otherwise, SQLModel might fail to initialize relationships properly
 # for more details: https://github.com/tiangolo/full-stack-fastapi-template/issues/28
+USERS = [
+    {
+        "email": settings.FIRST_SUPERUSER,
+        "password": settings.FIRST_SUPERUSER_PASSWORD,
+        "is_superuser": True,
+        "tag": "admin",
+    },
+    {
+        "email": "user1@example.com",
+        "password": settings.FIRST_SUPERUSER_PASSWORD,
+        "is_superuser": False,
+        "tag": "user1",
+    },
+    {
+        "email": "user2@example.com",
+        "password": settings.FIRST_SUPERUSER_PASSWORD,
+        "is_superuser": False,
+        "tag": "user2",
+    }
+]
+
+
+def _create_init_data(session: Session) -> None:
+    for user in USERS:
+        user_in = UserCreate(**user)
+        user = crud.create_user(session=session, user_create=user_in)
+
+    # create friendship between user1 and user2
+    user1 = crud.get_user_by_email(session=session, email="user1@example.com")
+    user2 = crud.get_user_by_email(session=session, email="user2@example.com")
+    friendship = Friend(user_1_id=user1.id, user_2_id=user2.id, invited_by=user1.id, accepted=True)
+    session.add(friendship)
+    session.commit()
 
 
 def init_db(session: Session) -> None:
@@ -25,11 +64,8 @@ def init_db(session: Session) -> None:
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
     ).first()
+
+    # if the first user doesn't exist, the database is probably empty
     if not user:
-        user_in = UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-            tag="admin",
-        )
-        user = crud.create_user(session=session, user_create=user_in)
+        _create_init_data(session)
+
