a00911506b0e58984e0c7542d87ca5e03a5599e8
maciek <macieq2048@gmail.com>
2024-07-02 22:46:29 +0200

    feat: update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,19 @@
 # Exit in case of error
 set -e
 
-TAG=${TAG?Variable not set} \
-FRONTEND_ENV=${FRONTEND_ENV-production} \
-docker-compose \
--f docker-compose.yml \
-build
+# export TAG=latest
+# ./scripts/build.sh
+# docker tag backend:latest 972830530698.dkr.ecr.eu-central-1.amazonaws.com/snapi-backend:latest
+# docker push 972830530698.dkr.ecr.eu-central-1.amazonaws.com/snapi-backend:latest
+#TAG=${TAG?Variable not set} \
+#docker-compose \
+#-f docker-compose.yml \
+#build
+
+# admin@snapi.com:upt5LU0KbNF9QQV1Nnxz9
+
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 972830530698.dkr.ecr.eu-central-1.amazonaws.com
+
+cd backend
+docker build . -t 972830530698.dkr.ecr.eu-central-1.amazonaws.com/snapi-backend:latest
+docker push 972830530698.dkr.ecr.eu-central-1.amazonaws.com/snapi-backend:latest
